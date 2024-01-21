8c44de335dfa378de58f4a7b1b4d7b0abb2bce6b
Lucas-ifro <lucaslfr00@gmail.com>
2024-01-21 13:42:46 -0500

    projeto crate-react

---
+++ reactjs/dtmoney/src/App.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+const Title = styled.h1`
+font-size: 64px;
+color: #8257e6;
+`
+
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>
+        <Title>
+        Hello world
+        </Title>
+      </h1>
+    </div>
+  );
+}
+
