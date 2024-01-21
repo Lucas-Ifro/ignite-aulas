8fe653284f507d34ff9472b66c06e28545e17872
Lucas-ifro <lucaslfr00@gmail.com>
2024-01-21 16:50:27 -0500

    corrigindo erros

--- reactjs/dtmoney/src/App.tsx
+++ reactjs/dtmoney/src/App.tsx
@@ -1,20 +1,9 @@
-import styled from "styled-components"
-
-const Title = styled.h1`
-font-size: 64px;
-color: #8257e6;
-`
-
-
 export function App() {
   return (
     <div className="App">
-      <h1>
-        <Title>
-        Hello world
-        </Title>
-      </h1>
+      <h1>hello world</h1>
     </div>
   );
 }
+
 
