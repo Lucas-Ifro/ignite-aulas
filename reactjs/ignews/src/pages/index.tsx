67348a0c450f9785280896c50c3219925894eef2
lucas Ferreira <lucaslfr00@gmail.com>
2024-02-03 16:17:14 -0300

    pagina home finalizada

--- reactjs/ignews/src/pages/index.tsx
+++ reactjs/ignews/src/pages/index.tsx
@@ -1,12 +1,26 @@
 import Head from "next/head"
-
+import styles from './Home.module.scss'
 
 export default function Home() {
   return (
     <>
     <Head>
-    <title>IG.news</title>
+    <title>Home | ig.news</title>
     </Head>
+
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <span>👏 Hey, Welcome</span>
+        <h1>News abaut the <span>React</span> world.</h1>
+        <p>
+          Get access to all the publications <br />
+          <span>for $9.90 month</span>
+        </p>
+      </section>
+
+      <img src="/imagens/avatar.svg" alt="Mulher programando" />
+
+    </main>
     </>
   );
 }
