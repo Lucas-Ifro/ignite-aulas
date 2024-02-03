859bd98c4d154aea120982f1e40f1f8a64674a91
lucas Ferreira <lucaslfr00@gmail.com>
2024-02-02 21:00:24 -0300

    SigInButton finalizado

---
+++ reactjs/ignews/src/componests/SignInButton/index.tsx
@@ -0,0 +1,32 @@
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi';
+
+import styles from './styles.module.scss'
+
+export function SignInButton() {
+    const isUserLoggedIn = true;
+
+    return isUserLoggedIn ? (
+        
+        <button 
+            type='button'
+            className={styles.signinButton}
+        >
+            <FaGithub color='#04d361'/>
+            Lucas Ferreira
+
+            <FiX color='#737380' className={styles.closeIcon} />
+        </button>
+
+    ) : (
+        
+        <button 
+            type='button'
+            className={styles.signinButton}
+        >
+            <FaGithub color='#eba417'/>
+            Sign in with Github
+        </button>
+
+    )
+}
