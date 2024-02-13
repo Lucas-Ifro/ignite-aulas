08c50371ae604f48059bbcc0b1dc6c8dbb74a4ca
lucas Ferreira <lucaslfr00@gmail.com>
2024-02-13 19:24:17 -0300

    Configurando o faunaDB

--- reactjs/ignews/src/pages/api/auth/[...nextauth].ts
+++ reactjs/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,10 @@
+import { query as q } from "faunadb"
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from "../../../services/fauna"
+
 
 export const authOptions = {
   providers: [
@@ -9,5 +14,20 @@ export const authOptions = {
       scope: 'read:user'
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      console.log(user)
+      const { email } = user
+
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          {data: { email } }
+        )
+      )
+
+      return true
+    }
+  }
 }
 export default NextAuth(authOptions)
